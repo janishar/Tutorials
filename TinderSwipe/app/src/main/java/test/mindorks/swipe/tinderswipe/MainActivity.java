b9cc96601c261f21d1e4b11f8bb3740edaeb6e5d
janishar <janishar.ali@gmail.com>
2016-10-08 19:15:13 +0530

    new version

--- TinderSwipe/app/src/main/java/test/mindorks/swipe/tinderswipe/MainActivity.java
+++ TinderSwipe/app/src/main/java/test/mindorks/swipe/tinderswipe/MainActivity.java
@@ -30,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
                 .setHeightSwipeDistFactor(10)
                 .setWidthSwipeDistFactor(5)
                 .setSwipeDecor(new SwipeDecor()
-                .setViewWidth(windowSize.x)
-                .setViewHeight(windowSize.y - bottomMargin)
-                .setViewGravity(Gravity.TOP)
-                .setPaddingTop(20)
-                .setRelativeScale(0.01f)
+                        .setViewWidth(windowSize.x)
+                        .setViewHeight(windowSize.y - bottomMargin)
+                        .setViewGravity(Gravity.TOP)
+                        .setPaddingTop(20)
+                        .setRelativeScale(0.01f)
                 .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
                 .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
 
