3502a18a0f538a03007d80144ba947872b9da30c
janishar <janishar.ali@gmail.com>
2017-12-02 01:09:08 +0530

    Add Blur On Below Cards

--- TinderSwipe/app/src/main/java/test/mindorks/swipe/tinderswipe/TinderCard.java
+++ TinderSwipe/app/src/main/java/test/mindorks/swipe/tinderswipe/TinderCard.java
@@ -6,18 +6,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.MultiTransformation;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
 import com.mindorks.placeholderview.annotations.Click;
 import com.mindorks.placeholderview.annotations.Layout;
-import com.mindorks.placeholderview.annotations.NonReusable;
 import com.mindorks.placeholderview.annotations.Resolve;
 import com.mindorks.placeholderview.annotations.View;
 import com.mindorks.placeholderview.annotations.swipe.SwipeCancelState;
+import com.mindorks.placeholderview.annotations.swipe.SwipeHead;
 import com.mindorks.placeholderview.annotations.swipe.SwipeIn;
 import com.mindorks.placeholderview.annotations.swipe.SwipeInState;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOutState;
+import com.mindorks.placeholderview.annotations.swipe.SwipeView;
 
+import jp.wasabeef.glide.transformations.BlurTransformation;
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 /**
@@ -35,6 +38,9 @@ public class TinderCard {
     @View(R.id.locationNameTxt)
     private TextView locationNameTxt;
 
+    @SwipeView
+    private android.view.View cardView;
+
     private Profile mProfile;
     private Context mContext;
     private SwipePlaceHolderView mSwipeView;
@@ -47,12 +53,27 @@ public class TinderCard {
 
     @Resolve
     private void onResolved(){
+        MultiTransformation multi = new MultiTransformation(
+                new BlurTransformation(mContext, 30),
+                new RoundedCornersTransformation(
+                        mContext, Utils.dpToPx(7), 0,
+                        RoundedCornersTransformation.CornerType.TOP));
+
         Glide.with(mContext).load(mProfile.getImageUrl())
-                .bitmapTransform(new RoundedCornersTransformation(mContext, Utils.dpToPx(7), 0,
-                        RoundedCornersTransformation.CornerType.TOP))
+                .bitmapTransform(multi)
                 .into(profileImageView);
         nameAgeTxt.setText(mProfile.getName() + ", " + mProfile.getAge());
         locationNameTxt.setText(mProfile.getLocation());
+    }
+
+    @SwipeHead
+    private void onSwipeHeadCard() {
+        Glide.with(mContext).load(mProfile.getImageUrl())
+                .bitmapTransform(new RoundedCornersTransformation(
+                        mContext, Utils.dpToPx(7), 0,
+                        RoundedCornersTransformation.CornerType.TOP))
+                .into(profileImageView);
+        cardView.invalidate();
     }
 
     @Click(R.id.profileImageView)
