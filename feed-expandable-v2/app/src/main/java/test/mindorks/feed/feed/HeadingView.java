b019cefc2b3d808b88799d9011b8e414a2c29929
Ali <janishar.ali@gmail.com>
2018-03-04 15:55:40 +0530

    Create Project

---
+++ feed-expandable-v2/app/src/main/java/test/mindorks/feed/feed/HeadingView.java
@@ -0,0 +1,62 @@
+package test.mindorks.feed.feed;
+
+import android.content.Context;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+import com.mindorks.placeholderview.annotations.expand.Collapse;
+import com.mindorks.placeholderview.annotations.expand.Expand;
+import com.mindorks.placeholderview.annotations.expand.Parent;
+import com.mindorks.placeholderview.annotations.expand.ParentPosition;
+import com.mindorks.placeholderview.annotations.expand.SingleTop;
+import com.mindorks.placeholderview.annotations.expand.Toggle;
+
+/**
+ * Created by janisharali on 24/08/16.
+ */
+
+@Parent
+@SingleTop
+@Layout(R.layout.feed_heading)
+public class HeadingView {
+
+    @View(R.id.headingTxt)
+    TextView headingTxt;
+
+    @View(R.id.toggleIcon)
+    ImageView toggleIcon;
+
+    @Toggle(R.id.toggleView)
+    LinearLayout toggleView;
+
+    @ParentPosition
+    int mParentPosition;
+
+    private Context mContext;
+    private String mHeading;
+
+    public HeadingView(Context context, String heading) {
+        mContext = context;
+        mHeading = heading;
+    }
+
+    @Resolve
+    public void onResolved() {
+        toggleIcon.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_keyboard_arrow_up_white_24dp));
+        headingTxt.setText(mHeading);
+    }
+
+    @Expand
+    public void onExpand() {
+        toggleIcon.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_keyboard_arrow_down_white_24dp));
+    }
+
+    @Collapse
+    public void onCollapse() {
+        toggleIcon.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_keyboard_arrow_up_white_24dp));
+    }
+}
---
+++ feed-expandable-v2/app/src/main/java/test/mindorks/feed/feed/MainActivity.java
@@ -0,0 +1,28 @@
+package test.mindorks.feed.feed;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mindorks.placeholderview.ExpandablePlaceHolderView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ExpandablePlaceHolderView mExpandableView;
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = this.getApplicationContext();
+        mExpandableView = (ExpandablePlaceHolderView)findViewById(R.id.expandableView);
+
+        for (Feed feed : Utils.loadFeeds(this.getApplicationContext())) {
+            mExpandableView.addView(new HeadingView(mContext, feed.getHeading()));
+            for (Info info : feed.getInfoList()) {
+                mExpandableView.addView(new InfoView(mContext, info));
+            }
+        }
+    }
+}
